c47d2d12097a01b4f6a4ae999411ea5dce3834c6
alex <alex@2DAM-011>
2025-10-29 08:43:30 +0100

    My first commit on dev_feature2

--- ProyectoSignIn/src/ProyectoSignInApplication.java
+++ ProyectoSignIn/src/ProyectoSignInApplication.java
@@ -10,7 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import proyectosignin1.ui.GestionUsuariosController;
+import ui.GestionUsuariosController;
 
 /**
  *
